860aeba8ccef9160737f1e5ac391b410ae6872e9
hackwellbn <119762901+hackwellbn@users.noreply.github.com>
2025-03-10 10:08:10 +0000

    adds numbers

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+let num1 = 31, num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 14, num4 = 5;
+let mod = num3 % num4;
+
+let numbers = [5, 12, 20, 18, 3];
+let max = Math.max(...numbers);
